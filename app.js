bf5451a1e6f73f0e917c1fc334ddf568506e6598
tandang <cuctrau159@gmai.com>
2022-10-14 03:26:13 +0700

    v5

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const connectDB = require('./src/config/connectDB')
-// const redisService = require('./src/services/redisService');
-const redisDb = require('./src/app/redis');
 const app = express()
 const useragent = require('express-useragent');
 // Connect to MongoDB
@@ -29,17 +27,10 @@ const io = socketio(server);
 socket(io);
 app.use(handleErr);
 
-app.use('/api', (res,req)=>{
-    res.send("hello")
-});
-
 routes(app,io);
 
 
-
 const port = process.env.PORT
-
-
 
 server.listen(port, () => {
     console.log('Example app listening on http://localhost:'+port)
